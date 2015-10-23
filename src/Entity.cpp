9ddc797d5c7a4b6041a35787cd50112ceff83dc4
Niko Salakka <nsalakka@gmail.com>
2015-10-23 18:41:34 +0300

    askjfh

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -15,8 +15,11 @@ Entity::~Entity(){
 
 void Entity::Init() {
     MakeTestEntity();
-    renderEntity.shader = LoadShader("shaders/test_vert.glsl"
-                                   , "shaders/test_frag.glsl");
+#ifdef _WIN32
+    renderEntity.shader = LoadShader("..\shaders\test_vert.glsl" , "..\shaders\test_frag.glsl");
+#else
+    renderEntity.shader = LoadShader("shaders/test_vert.glsl" , "shaders/test_frag.glsl");
+#endif
 }
 
 void Entity::Spawn() {
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -33,23 +33,14 @@ void Renderer::RenderEntities(std::vector<Entity>* entities) {
 void Renderer::RenderEntity(renderEntity_t entity, int* pointcount) {
 
     std::vector<vector3<GLfloat> > vertexData;
-    std::vector<vector3<GLfloat> > colorData;
 
-    int vertCount = entity.vertices.size() * 6;
-    GLfloat vertices[vertCount];
-    
     int i = 0;
     for (auto v : entity.vertices) {
-        vertices[i] = v.position.x;
-        vertices[i+1] = v.position.y;
-        vertices[i+2] = v.position.z;
-        vertices[i+3] = v.color.x;
-        vertices[i+4] = v.color.y;
-        vertices[i+5] = v.color.z;
-        i += 6;
+		vertexData.push_back(vector3<GLfloat>(v.position.x , v.position.y , v.position.z));
+		vertexData.push_back(vector3<GLfloat>(v.color.x , v.color.y , v.color.z));
     }
 
-    *pointcount += vertCount / 2;
+    *pointcount += vertexData.size() / 2 * 3;
 
 	GLuint vao = 0;
 	glGenVertexArrays(1, &vao);
@@ -58,16 +49,16 @@ void Renderer::RenderEntity(renderEntity_t entity, int* pointcount) {
 	GLuint vbo = 0;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertexData.size(), vertexData.data(), GL_STATIC_DRAW);
 
     glUseProgram(entity.shader);
 
     GLint posAttrib = glGetAttribLocation(entity.shader, "position");
 	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), 0);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 2*sizeof(vector3<GLfloat>), 0);
 
     GLint colAttrib = glGetAttribLocation(entity.shader, "color");
 	glEnableVertexAttribArray(colAttrib);
 	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE,
-                          6*sizeof(GLfloat),(void*)(3*sizeof(GLfloat)));
+		2 * sizeof(vector3<GLfloat>),(void*)(sizeof(vector3<GLfloat>)));
 }
